f703aca817ff485556c361348003eac5216483de
catherinekimani <catherinekimani882@gmail.com>
2022-09-23 08:50:00 +0100

    ft:Add media query for tablet view

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -75,14 +75,14 @@ const End = styled.div`
 const Landing = () => {
     return (
         <div className='container-fluid'>
-            <nav class="navbar navbar-dark navbar-expand-md ">
+            <nav class="navbar navbar-dark navbar-expand-lg ">
                 <div class="container-fluid">
                     <div class='logo d-flex'>
                     <img src={logo} alt='logo' ></img>
                         <Button1><li class="nav-item"><a class="nav-link btn1 text-light" href='join'>Hosterr is Hiring!</a></li></Button1>
                         </div>
                     {/* collapse */}
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon bg-dark"></span>
                     </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
